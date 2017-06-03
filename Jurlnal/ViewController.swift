4a2bb6694e92deb044e407770bfea66ab2f23710
Leo Dion <leogdion@brightdigit.com>
2017-06-02 21:11:46 -0400

    Initial Commit

---
+++ Jurlnal/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jurlnal
+//
+//  Created by Leo Dion on 6/2/17.
+//  Copyright © 2017 Leo Dion. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
